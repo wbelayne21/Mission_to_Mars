ab1bae82735fb30477a3e7486548b423c1858eaf
wbelayne21 <wastexmamo@gmail.com>
2022-01-09 22:34:33 -0500

    Deliverables 1, 2 and 3

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
@@ -10,18 +9,17 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 
-app.route("/")
+@app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars = mars)
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
-app.route("/scrape")
+@app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update_one({}, {"$set":mars_data}, upsert=True)
-    return redirect('/, code = 302')
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
 
 if __name__ == "__main__":
-    app.run()
-
+   app.run()
